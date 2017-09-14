a1a6350cfe19907ef3c3fd62c540ae6be7a7568b
redaxmedia <info@redaxmedia.com>
2017-09-14 15:12:06 +0200

    Unit testing for settings model

---
+++ tests/phpunit/Model/SettingTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace Redaxscript\Tests\Module;
+
+use Redaxscript\Model;
+use Redaxscript\Tests\TestCaseAbstract;
+
+/**
+ * SettingTest
+ *
+ * @since 3.3.0
+ *
+ * @package Redaxscript
+ * @category Tests
+ * @author Henry Ruhs
+ */
+
+class SettingTest extends TestCaseAbstract
+{
+	/**
+	 * setUp
+	 *
+	 * @since 3.3.0
+	 */
+
+	public function setUp()
+	{
+		parent::setUp();
+		$installer = $this->installerFactory();
+		$installer->init();
+		$installer->rawCreate();
+		$installer->insertSettings(
+		[
+			'adminName' => 'Test',
+			'adminUser' => 'test',
+			'adminPassword' => 'test',
+			'adminEmail' => 'test@test.com'
+		]);
+	}
+
+	/**
+	 * tearDown
+	 *
+	 * @since 3.3.0
+	 */
+
+	public function tearDown()
+	{
+		$installer = $this->installerFactory();
+		$installer->init();
+		$installer->rawDrop();
+	}
+
+	/**
+	 * testGetAndSetSetting
+	 *
+	 * @since 2.2.0
+	 */
+
+	public function testGetAndSetSetting()
+	{
+		/* setup */
+
+		$settingModel = new Model\Setting();
+		$settingModel->setSetting('charset', 'utf-16');
+
+		/* actual */
+
+		$actual = $settingModel->getSetting('charset');
+
+		/* compare */
+
+		$this->assertEquals('utf-16', $actual);
+	}
+
+	/**
+	 * testGetSettingInvalid
+	 *
+	 * @since 2.2.0
+	 */
+
+	public function testGetSettingInvalid()
+	{
+		/* setup */
+
+		$settingModel = new Model\Setting();
+
+		/* actual */
+
+		$actual = $settingModel->getSetting('invalidKey');
+
+		/* compare */
+
+		$this->assertFalse($actual);
+	}
+}
